8a46f2cbd68126c0518104f706988703d46f4d97
felixhalim <felixh@comp.nus.edu.sg>
2022-01-08 12:17:13 +0800

    Edit image outliner

--- imageOutliner.py
+++ imageOutliner.py
@@ -4,13 +4,23 @@ import pathlib
 
 def main():
     rootDir = "{}\img".format(pathlib.Path().resolve())
+    error = []
     for dirName, _, fileList in os.walk(rootDir):
         for fname in fileList:
             fullPath = "{}\{}".format(dirName, fname)
-            path = "{}_proc".format(dirName)
+            path = "{}_proc".format(dirName.replace("+","_"))
+            print(fullPath)
+            print(path)
             isExist = os.path.exists(path)
-            if not isExist:
+            if not isExist and "_proc" not in fullPath:
                 os.mkdir(path)
-            sketch.normalsketch(fullPath, path, "{}.jpg".format(fname))
+            try:
+                sketch.normalsketch(fullPath, path, fname, scale=10)
+            except:
+                error.append((fullPath, path, fname))
+                print("fail", fullPath, path, fname)
+    print(len(error))
+    print(error)
+
 
 main()
